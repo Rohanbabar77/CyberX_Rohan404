a9f895502914d5014c26002402b8904d4a6ae0bb
HackerX_Rohan404 <rohanbabar2010@gmail.com>
2025-07-18 13:47:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const audio = new Audio("music.mp3"); // replace with your music file
+  const audio = new Audio("Sayfalse_Junior_RCE_-_FUNK_MI_CAMINO_(FeelMP3.com).mp3"); // replace with your music file
   audio.volume = 1.0;
   audio.loop = true;
   audio.play().catch((err) => {
