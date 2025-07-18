11409678284e9e3790dd3cbf616a701c7340442f
HackerX_Rohan404 <rohanbabar2010@gmail.com>
2025-07-19 01:00:20 +0530

    Delete admin-dashboard.js

--- admin-dashboard.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('totalVisits').textContent = "2314";
-  document.getElementById('uniqueVisitsToday').textContent = "127";
-  document.getElementById('messagesReceived').textContent = "52";
-});
