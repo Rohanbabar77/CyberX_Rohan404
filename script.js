da84f8469f8d3465ddd3f5bb9144ec1b8869a6d7
HackerX_Rohan404 <rohanbabar2010@gmail.com>
2025-07-18 14:30:00 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const audio = new Audio("Sayfalse_Junior_RCE_-_FUNK_MI_CAMINO_(FeelMP3.com).mp3");
-audio.volume = 1.0;
-audio.autoplay = true;
-audio.loop = true;
-document.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    audio.play().catch(e => console.log("Autoplay failed"));
-  }, 1000);
-});
