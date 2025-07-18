c563ecd333c99d076e44a0b9cd8e625bb9aa7917
HackerX_Rohan404 <rohanbabar2010@gmail.com>
2025-07-18 14:24:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const audio = new Audio("music.mp3");
+const audio = new Audio("Sayfalse_Junior_RCE_-_FUNK_MI_CAMINO_(FeelMP3.com).mp3");
 audio.volume = 1.0;
 audio.autoplay = true;
 audio.loop = true;
