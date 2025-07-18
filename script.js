72d7d2f545b06dc346aace3b06c2716237d72aeb
HackerX_Rohan404 <rohanbabar2010@gmail.com>
2025-07-19 00:04:10 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", () => {
-  const audio = new Audio("your-music.mp3");
-  audio.volume = 1.0;
-  audio.loop = true;
-  audio.play().catch((e) => {
-    console.warn("Autoplay failed:", e);
-  });
-});
